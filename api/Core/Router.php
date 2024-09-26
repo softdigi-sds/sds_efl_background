d895ef662f7989c96bda18a0a31816e60233a661
Raju10136 <mythri.raja@gmail.com>
2024-09-26 19:52:10 +0530

    Update Router.php

--- api/Core/Router.php
+++ api/Core/Router.php
@@ -45,21 +45,23 @@ class Router
         // Get the current URL
         $this->_currentUrl = $_SERVER['REQUEST_URI'];
         //
-        $this->_currentUrl = str_replace("/api","",$this->_currentUrl);
+        $this->_currentUrl = str_replace("/api", "", $this->_currentUrl);
         // get routes
         $this->get_routes();
         // process and return routes
         return $this->process_route();
     }
 
+
     private function get_routes()
     {
         $this->_routes = SiteIndexRouter::getRoutes();
         //
-       // var_dump($this->_routes);
+        // var_dump($this->_routes);
     }
 
-    private function pattern_to_regular_expression($pattern_input){
+    private function pattern_to_regular_expression($pattern_input)
+    {
         // Convert the pattern into a regular expression
         $pattern = preg_replace('/\{(\w+)\}/', '(?P<\1>[^/]+)', $pattern_input);
         $pattern = str_replace('/', '\/', $pattern);
@@ -74,14 +76,14 @@ class Router
             // Convert the pattern into a regular expression
             $pattern = $this->pattern_to_regular_expression($pattern);
             //
-          //  echo "pattern = "  . $pattern . "<br/>";
-          //  echo "url " . $this->_currentUrl . "<br/>";
+            //  echo "pattern = "  . $pattern . "<br/>";
+            //  echo "url " . $this->_currentUrl . "<br/>";
             // Check if the URL matches the pattern
             if (preg_match($pattern, $this->_currentUrl, $matches)) {
                 // Extract named parameters from the match
                 $parameters = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
                 //
-                return [$controllerArr,$parameters];
+                return [$controllerArr, $parameters];
                 // 
                 break;
             }
