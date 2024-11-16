08a57e2ef6c99515240e36dc92a8b64e2943938d
Raju10136 <mythri.raja@gmail.com>
2024-11-16 18:07:08 +0530

    production

--- api/Site/Controller/PaymentController.php
+++ api/Site/Controller/PaymentController.php
@@ -57,43 +57,47 @@ class PaymentController extends BaseController
         $this->response($data);
     }
 
-    public function getAllReport(){
+    public function getAllReport()
+    {
         $bill_id = SmartData::post_select_value("sd_bill_id");
         $sd_customer_id = SmartData::post_select_value("sd_customer_id");
         $sd_hub_id = SmartData::post_select_value("sd_hub_id");
         $sql_arr = [];
         $data_in = [];
-        if($bill_id > 0){
+        if ($bill_id > 0) {
             $sql_arr[] = "t1.sd_bill_id=:bill_id";
-            $data_in["bill_id"] = $bill_id;   
+            $data_in["bill_id"] = $bill_id;
         }
-        if($sd_customer_id > 0){
+        if ($sd_customer_id > 0) {
             $sql_arr[] = "t1.sd_customer_id=:sd_customer_id";
-            $data_in["sd_customer_id"] = $sd_customer_id;   
+            $data_in["sd_customer_id"] = $sd_customer_id;
         }
-        if($sd_hub_id > 0){
+        if ($sd_hub_id > 0) {
             $sql_arr[] = "t1.sd_hub_id=:sd_hub_id";
-            $data_in["sd_hub_id"] = $sd_hub_id;   
+            $data_in["sd_hub_id"] = $sd_hub_id;
         }
-        $sql = implode(" AND ",$sql_arr);
-        $_invoice_data = $this->_invoice_helper->getAllReport($sql,$data_in);
+        $sql = implode(" AND ", $sql_arr);
+        $_invoice_data = $this->_invoice_helper->getAllReport($sql, $data_in);
         $_data =  new \stdClass();
         $_data->invoice_count = count($_invoice_data);
         $_data->invoice_amount = 0;
         $_data->paid_amount = 0;
         $_data->rem_amount = 0;
         // loop over invoice data 
-        foreach($_invoice_data as $obj){
+        foreach ($_invoice_data as $obj) {
             $obj->rem = $obj->total_amount - floatval($obj->paid);
             $_data->invoice_amount += $obj->total_amount;
-            $_data->paid_amount +=floatval($obj->paid);
-            $_data->rem_amount  +=  $obj->rem ;
+            $_data->paid_amount += floatval($obj->paid);
+            $_data->rem_amount  +=  $obj->rem;
         }
+        $_data->paid_amount = round($_data->paid_amount, 2);
+        $_data->invoice_amount = round($_data->invoice_amount, 2);
+        $_data->rem_amount = round($_data->rem_amount, 2);
         $out = new \stdClass();
         $out->_invoice_data = $_invoice_data;
         $out->_payment_data = $_data;
         //
-        $this->response( $out );
+        $this->response($out);
     }
 
     /**
